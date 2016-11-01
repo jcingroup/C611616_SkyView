f1cc0d11ea8f79a82402f2df46d9f84e5d4704cf
TinWu <yyuuix@gmail.com>
2016-11-01 17:15:14 +0800

    加入專案檔案。

---
+++ SkyView/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkyView.Startup))]
+namespace SkyView
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
