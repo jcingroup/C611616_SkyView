c2ddb33ce886ef9576fd27fa2305fdc20722a7e8
Gary Chang <gary661116@gmail.com>
2016-11-21 17:23:17 +0800

    新增後台景點資訊及圖片

--- SkyView/Controllers/ManageController.cs
+++ SkyView/Controllers/ManageController.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebUser.Controller;
+using SkyView.Service;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+/*Json.NET相關的命名空間*/
+using Newtonsoft.Json;
 
 namespace SkyView.Controllers
 {
     public class ManageController : WebUserController
     {
+        OverlookDBService OverlookDB = new OverlookDBService();
+
         // 後台首頁-導向Login
         public ActionResult Index()
         {
@@ -24,17 +35,220 @@ namespace SkyView.Controllers
         // 空拍
         public ActionResult OverlookList()
         {
-            return View();
-        }
-        public ActionResult OverlookData()
-        {
+            //OverlookDBService OverlookDB = new OverlookDBService();
+            DataTable dt = OverlookDB.List("", "");
+            ViewData["dt"] = dt;
             return View();
         }
 
+        //新增
+        public ActionResult Add()
+        {
+            //OverlookDBService OverlookDB = new OverlookDBService();
+            DataTable d_area = OverlookDB.AreaList();
+            DataTable d_scenic = OverlookDB.List("", "");
+            DataTable d_scenic_img_b = OverlookDB.Img_List("", "B");
+            DataTable d_scenic_img_s = OverlookDB.Img_List("", "S");
+
+            ViewData["d_area"] = d_area;
+            ViewData["d_scenic"] = d_scenic;
+            ViewData["d_scenic_img_b"] = d_scenic_img_b;
+            ViewData["d_scenic_img_s"] = d_scenic_img_b;
+            ViewData["action_sty"] = "add";
+            
+            return View("OverlookData");
+        }
+
+        //修改
+        public ActionResult Edit(string scenic_id = "")
+        {
+            //OverlookDBService OverlookDB = new OverlookDBService();
+            DataTable d_area = OverlookDB.AreaList();
+            DataTable d_scenic = OverlookDB.List(scenic_id, "");
+            DataTable d_scenic_img_b = OverlookDB.Img_List(scenic_id,"B");
+            DataTable d_scenic_img_s = OverlookDB.Img_List(scenic_id, "S");
+            ViewData["d_area"] = d_area;
+            ViewData["d_scenic"] = d_scenic;
+            ViewData["d_scenic_img_b"] = d_scenic_img_b;
+            ViewData["d_scenic_img_s"] = d_scenic_img_s;
+            ViewData["action_sty"] = "edit";
+
+            return View("OverlookData");
+        }
+
+        public ActionResult Del(string scenic_id = "")
+        {
+            //OverlookDBService OverlookDB = new OverlookDBService();
+            OverlookDB.Del(scenic_id);
+            return RedirectToAction("OverlookList");
+        }
+
+        [HttpPost]
+        public ActionResult Save(string action_sty, string area_id, string scenic_id, string scenic_name, string longitude, string latitude, string scenic_desc,string show,string img_no)
+        {
+            //OverlookDBService OverlookDB = new OverlookDBService();
+
+            switch (action_sty)
+            {
+                case "add":
+                    OverlookDB.Insert(area_id, scenic_name, longitude, latitude, scenic_desc,show,img_no);
+                    break;
+                case "edit":
+                    OverlookDB.Update(scenic_id, area_id, scenic_name, longitude, latitude, scenic_desc,show);
+                    break;
+            }
+
+            return RedirectToAction("OverlookList");
+        }
+
+        [HttpPost]
+        public ActionResult Login_Chk(string account="", string pwd="")
+        {
+            try
+            {
+                return RedirectToAction("OverlookList");
+            }
+            catch
+            {
+                return View();
+            }
+        }
         // 變更密碼
         public ActionResult ChangePW()
         {
             return View();
         }
+
+        public ActionResult Upload(string img_no,string img_sta)
+        {
+            DataTable img_file;
+            DataTable chk_file;
+
+            HttpFileCollection hfc = System.Web.HttpContext.Current.Request.Files;
+            string imgPath = "";
+            string filename = "";
+            string file_name = "";
+            string file_path = "../Images/";
+            string str_return = "";
+            string[] files;
+            string chk_sty = "";
+            string pre_filename = "";
+            int files_count = 0;
+
+            if (hfc.Count > 0)
+            {
+                    file_name = hfc[0].FileName;
+                    files = file_name.Split('\\');
+                    files_count = files.Count();
+                    filename = files[files_count - 1];
+                    
+                    imgPath = file_path + img_no + "_" + img_sta + "_" + filename;
+                    string PhysicalPath = Server.MapPath(imgPath);
+                    hfc[0].SaveAs(PhysicalPath);
+            }
+
+            chk_file = OverlookDB.Img_List(img_no, img_sta);
+
+            chk_sty = "add";
+            if(img_sta == "S")
+            {
+                if (chk_file.Rows.Count > 0)
+                {
+                    pre_filename = file_path + chk_file.Rows[0]["img_file"].ToString();
+
+                    chk_sty = "update";
+                }
+            }
+
+            switch(chk_sty)
+            {
+                case "add": //加入到資料庫
+                    OverlookDB.Img_Insert(img_no, img_no + "_" + img_sta + "_" + filename, img_sta);
+                    break;
+                case "update":
+                    OverlookDB.Img_Update(chk_file.Rows[0]["_SEQ_ID"].ToString(), img_no + "_" + img_sta + "_" + filename);
+
+                    //刪除原本檔案
+                    string Pre_Path = Server.MapPath(pre_filename);
+
+                    // Delete a file by using File class static method...
+                    if (System.IO.File.Exists(Pre_Path))
+                    {
+                        // Use a try block to catch IOExceptions, to
+                        // handle the case of the file already being
+                        // opened by another process.
+                        try
+                        {
+                            System.IO.File.Delete(Pre_Path);
+                        }
+                        catch (System.IO.IOException e)
+                        {
+                            str_return = str_return + e.Message;
+                        }
+                    }
+                    break;
+            }
+
+            //抓取資料
+            img_file = OverlookDB.Img_List(img_no, img_sta);
+
+            str_return = JsonConvert.SerializeObject(img_file, Newtonsoft.Json.Formatting.Indented);
+
+            return Content(str_return);
+         
+
+        }
+
+        public ActionResult Img_Del(string img_id,string img_sta,string img_no)
+        {
+            string str_return = "";
+            DataTable img_file;
+            DataTable chk_file;
+            string filename = "";
+            string file_path = "../Images/";
+            string imgPath = "";
+
+            chk_file = OverlookDB.Img_List(img_no, img_sta);
+            filename = "";
+            if (chk_file.Rows.Count > 0)
+            {
+                for(int i = 0; i< chk_file.Rows.Count; i++)
+                {
+                    if(img_id == chk_file.Rows[i]["_SEQ_ID"].ToString())
+                    {
+                        filename = chk_file.Rows[i]["img_file"].ToString();
+                        break;
+                    }
+                }
+            }
+
+            imgPath = file_path + filename;
+
+            string PhysicalPath = Server.MapPath(imgPath);
+
+            // Delete a file by using File class static method...
+            if (System.IO.File.Exists(PhysicalPath))
+            {
+                // Use a try block to catch IOExceptions, to
+                // handle the case of the file already being
+                // opened by another process.
+                try
+                {
+                    System.IO.File.Delete(PhysicalPath);
+                }
+                catch (System.IO.IOException e)
+                {
+                    str_return = str_return + e.Message;
+                }
+            }
+
+            OverlookDB.Img_Delete(img_id);
+            //抓取資料
+            img_file = OverlookDB.Img_List(img_no, img_sta);
+            str_return = JsonConvert.SerializeObject(img_file, Newtonsoft.Json.Formatting.Indented);
+            return Content(str_return);
+        }
+
     }
+
 }
---
+++ SkyView/Service/OverlookDBService.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace SkyView.Service
+{
+    public class OverlookDBService
+    {
+        string conn_str = WebConfigurationManager.ConnectionStrings["conn_string"].ConnectionString.ToString();
+        string csql = "";
+        DataSet ds = new DataSet();
+
+        public DataTable List(string scenic_id = "",string sort = "")
+        {
+            SqlConnection conn = new SqlConnection(conn_str);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            csql = "select "
+                 + "  a.*, b.area_name "
+                 + "from "
+                 + "  scenic_bt a "
+                 + "left join area_bt b on a.area_id = b._SEQ_ID "
+                 + "where "
+                 + "  a.status <> 'D' ";
+            if(scenic_id.Trim().Length >0)
+            {
+                csql = csql + " and a._SEQ_ID = " + scenic_id + " ";
+            }
+
+            if(sort.Trim().Length > 0)
+            {
+                csql = csql + "order by " + sort + " ";
+            }
+
+            if(ds.Tables["scenic"] != null)
+            {
+                ds.Tables["scenic"].Clear();
+            }
+
+            SqlDataAdapter scenic_ada = new SqlDataAdapter(csql, conn);
+            scenic_ada.Fill(ds, "scenic");
+            scenic_ada = null;
+
+            if(conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            conn = null;
+
+            return ds.Tables["scenic"];
+        }
+
+        //新增
+        public string Insert(string area_id = "",string scenic_name = "", string longitude = "", string latitude = "",string scenic_desc = "",string show = "", string img_no = "")
+        {
+            string c_msg = "";
+            string scenic_id = "";
+            SqlConnection conn = new SqlConnection(conn_str);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            try
+            {
+                csql = "insert into scenic_bt(area_id,scenic_name,longitude,latitude,scenic_desc,status) "
+                     + "values(" + area_id + ",'" + scenic_name + "','" + longitude + "','" + latitude + "','" + scenic_desc + "','" + show + "')";
+
+                cmd.CommandText = csql;
+                cmd.ExecuteNonQuery();
+
+                //抓取其編號
+                csql = "select distinct "
+                     + "  _SEQ_ID "
+                     + "from "
+                     + "   scenic_bt "
+                     + "where "
+                     + "    area_id = " + area_id + " "
+                     + "and scenic_name = '" + scenic_name + "' "
+                     + "and longitude = '" + longitude + "' "
+                     + "and latitude = '" + latitude + "' "
+                     + "and scenic_desc = '" + scenic_desc + "' "
+                     + "and status = '" + show + "' ";
+                if(ds.Tables["chk_scenic"] != null)
+                {
+                    ds.Tables["chk_scenic"].Clear();
+                }
+
+                SqlDataAdapter scenic_chk_ada = new SqlDataAdapter(csql, conn);
+                scenic_chk_ada.Fill(ds, "chk_scenic");
+                scenic_chk_ada = null;
+
+                if(ds.Tables["chk_scenic"].Rows.Count > 0)
+                {
+                    scenic_id = ds.Tables["chk_scenic"].Rows[0]["_SEQ_ID"].ToString();
+                    if (img_no.Trim().Length > 0)
+                    {
+                        csql = "update "
+                             + "  scenic_img "
+                             + "set "
+                             + "  img_no = '" + scenic_id + "' "
+                             + "where "
+                             + "  img_no = '" + img_no + "' ";
+
+                        cmd.CommandText = csql;
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                c_msg = ex.Message;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                cmd = null;
+                conn = null;
+            }
+
+            return c_msg;
+        }
+
+        //更新內容
+        public string Update(string scenic_id = "",string area_id = "", string scenic_name = "", string longitude = "", string latitude = "", string scenic_desc = "",string show = "")
+        {
+            string c_msg = "";
+            SqlConnection conn = new SqlConnection(conn_str);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            try
+            {
+                csql = "update "
+                     + "  scenic_bt "
+                     + "set "
+                     + "  area_id = " + area_id + " "
+                     + ", scenic_name = '" + scenic_name + "' "
+                     + ", longitude = '" + longitude + "' "
+                     + ", latitude = '" + latitude + "' "
+                     + ", scenic_desc = '" + scenic_desc + "' "
+                     + ", status = '" + show + "' "
+                     + ", _UPD_ID = 'System' "
+                     + ", _UPD_DT = getdate() "
+                     + "where "
+                     + "  _SEQ_ID = " + scenic_id + " ";
+
+                cmd.CommandText = csql;
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                c_msg = ex.Message;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                cmd = null;
+                conn = null;
+            }
+
+            return c_msg;
+
+        }
+
+        public string Del(string scenic_id = "")
+        {
+            string c_msg = "";
+
+            SqlConnection conn = new SqlConnection(conn_str);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            try
+            {
+                csql = "delete from "
+                     + "  scenic_bt "
+                     + "where "
+                     + "  _SEQ_ID = " + scenic_id + " ";
+
+                cmd.CommandText = csql;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                c_msg = ex.Message;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                cmd = null;
+                conn = null;
+            }
+
+            return c_msg;
+        }
+        //更新狀態
+        public string Upd_Status(string scenic_id = "", string status = "")
+        {
+            string c_msg = "";
+
+            SqlConnection conn = new SqlConnection(conn_str);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            try
+            {
+                csql = "update "
+                     + "  scenic_bt "
+                     + "set "
+                     + "  status = '" + status + "' "
+                     + ", _UPD_ID = 'System' "
+                     + ", _UPD_DT = getdate() "
+                     + "where "
+                     + "  _SEQ_ID = " + scenic_id + " ";
+
+                cmd.CommandText = csql;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                c_msg = ex.Message;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                cmd = null;
+                conn = null;
+            }
+
+            return c_msg;
+        }
+
+        //區域資料
+        public DataTable AreaList()
+        {
+            SqlConnection conn = new SqlConnection(conn_str);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            csql = "select * from area_bt where status = 'Y' order by _SEQ_ID ";
+          
+
+            if (ds.Tables["area"] != null)
+            {
+                ds.Tables["area"].Clear();
+            }
+
+            SqlDataAdapter scenic_ada = new SqlDataAdapter(csql, conn);
+            scenic_ada.Fill(ds, "area");
+            scenic_ada = null;
+
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            conn = null;
+
+            return ds.Tables["area"];
+        }
+
+        public string Img_Insert(string img_no = "", string img_file = "", string img_sty = "")
+        {
+            string c_msg = "";
+            SqlConnection conn = new SqlConnection(conn_str);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            try
+            {
+                csql = "insert into scenic_img(img_no, img_file, img_sty) "
+                     + "values(" + img_no + ",'" + img_file + "','" + img_sty + "')";
+
+                cmd.CommandText = csql;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                c_msg = ex.Message;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                cmd = null;
+                conn = null;
+            }
+
+            return c_msg;
+        }
+
+        public string Img_Delete(string img_id = "")
+        {
+            string c_msg = "";
+            SqlConnection conn = new SqlConnection(conn_str);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            try
+            {
+                csql = "delete from scenic_img where _SEQ_ID = " + img_id + " ";
+
+                cmd.CommandText = csql;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                c_msg = ex.Message;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                cmd = null;
+                conn = null;
+            }
+
+            return c_msg;
+        }
+
+        public string Img_Update(string img_id = "", string img_file = "")
+        {
+            string c_msg = "";
+            SqlConnection conn = new SqlConnection(conn_str);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            try
+            {
+                csql = "update "
+                     + "  scenic_img "
+                     + "set "
+                     + "  img_file = '" + img_file + "' "
+                     + "where "
+                     + "  _SEQ_ID = " + img_id + " ";
+
+                cmd.CommandText = csql;
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                c_msg = ex.Message;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                cmd = null;
+                conn = null;
+            }
+
+            return c_msg;
+        }
+
+        public DataTable Img_List(string img_no = "",string img_sty = "B")
+        {
+            DataSet dt = new DataSet();
+            DataTable d_t;
+            SqlConnection conn = new SqlConnection(conn_str);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            csql = "select * from scenic_img where status = 'Y' and img_no = '" + img_no + "' and img_sty= '" + img_sty + "' order by _SEQ_ID ";
+
+
+            if (dt.Tables["img"] != null)
+            {
+                dt.Tables["img"].Clear();
+            }
+
+            SqlDataAdapter scenic_ada = new SqlDataAdapter(csql, conn);
+            scenic_ada.Fill(dt, "img");
+            scenic_ada = null;
+
+            d_t = dt.Tables["img"];
+
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+            conn = null;
+            dt = null;
+
+            return d_t;
+        }
+    }
+}
